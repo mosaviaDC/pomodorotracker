1f241dc790c0088e40df4d9535b0830fee24c147
Черный Денис <userden4ikwork@gmail.com>
2020-11-28 18:41:09 +0300

    change

--- src/components/AddTaskButtonContainer/AddTaskButtonContainer.jsx
+++ src/components/AddTaskButtonContainer/AddTaskButtonContainer.jsx
@@ -17,7 +17,7 @@ const AddTaskButtonContainer = () => {
     return <div className="AddTaskButtonContainer" id="addTaskButtonContainer" >
         <h1>Еще нет задач</h1>
         <p>Вы еще не создали ни одной задачи.</p>
-        <p>Коснитесь кнопки, что бы создать свою первую задачу</p>
+        <p>Коснитесь кнопки, что бы создать свою первую задачу!</p>
         <button type="button" className="btn btn-success btn-lg" onClick={AddTaskButtonClick}>Создать новую задау</button>
     </div>
 
