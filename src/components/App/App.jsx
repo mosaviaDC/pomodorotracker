e6c7f48846c5ffbb723bb044a388a20d4f7190ef
Черный Денис <userden4ikwork@gmail.com>
2020-11-29 20:46:38 +0300

    register add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,14 @@ import TasksPage from '../TasksPage/TasksPage';
 import Login from '../Login/Login'
 import { Switch, Route, Link } from "react-router-dom";
 import Register from '../Register/Register';
+import NavBar from '../NavBar/NavBar';
  const App = () => {
      return <div>
+         <NavBar/>
           <Switch>
-         <Route path="/tasks" component={TasksPage} />
-
+             <Route path="/tasks" component={TasksPage} />
+             <Route exact path="/signup" component={Register} />
+             <Route exact path="/login" component={Login} />
          </Switch>
          </div>
 }
