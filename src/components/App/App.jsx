3c855970eb3e74f790ad77c74d6aadc38ae57308
Черный Денис <userden4ikwork@gmail.com>
2020-11-29 20:18:35 +0300

    f

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,15 @@
 ﻿import React from 'react';
 import TasksPage from '../TasksPage/TasksPage';
-import Login from '../Login/Login'
+//import Login from '../Login/Login'
 import { Switch, Route, Link } from "react-router-dom";
-import Register from '../Register/Register';
+//import Register from '../Register/Register';
 import NavBar from '../NavBar/NavBar'
  const App = () => {
      return <div>
          <NavBar />,
           <Switch>
          <Route path="/tasks" component={TasksPage} />
-         <Route exact path="/login" component={Login} />
-         <Route exact path="/register" component={Register} />
+
          </Switch>
          </div>
 }
