9c8492ec4a5b92040af59c23d42586fe875f545f
Черный Денис <userden4ikwork@gmail.com>
2020-11-29 20:25:05 +0300

    trte

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,8 @@ import TasksPage from '../TasksPage/TasksPage';
 //import Login from '../Login/Login'
 import { Switch, Route, Link } from "react-router-dom";
 //import Register from '../Register/Register';
-import NavBar from '../NavBar/NavBar.jsx'
  const App = () => {
      return <div>
-         <NavBar />,
           <Switch>
          <Route path="/tasks" component={TasksPage} />
 
